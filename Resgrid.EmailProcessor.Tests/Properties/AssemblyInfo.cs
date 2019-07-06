c001cab6d681668d5f86a2b530deb4a438596575
Shawn Jackson <shawn@resgrid.com>
2019-07-05 19:06:50 -0700

    Added unit test project.

---
+++ Resgrid.EmailProcessor.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Resgrid.EmailProcessor.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Resgrid.EmailProcessor.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d9e570c-c282-4598-9d2a-cfed5b086b97")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Resgrid.EmailProcessor.Tests/SmtpTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Net.Mail;
+
+namespace Resgrid.EmailProcessor.Tests
+{
+	[TestFixture]
+	public class SmtpTests
+	{
+		[Ignore("")]
+		[Test]
+		public void SendTestEmail()
+		{
+			SmtpClient client = new SmtpClient();
+			client.Host = "";
+			client.Port = 81;
+
+			//create sender address
+			MailAddress from = new MailAddress("cad@dispatch.com", "Dispatch");
+
+			//create receiver address
+			MailAddress receiver = new MailAddress("xxxxx@testdispatch.resgrid.com", "Resgrid");
+
+			MailMessage message = new MailMessage(from, receiver);
+			message.Subject = "";
+			message.Body = "";
+
+			client.Send(message);
+		}
+	}
+}
